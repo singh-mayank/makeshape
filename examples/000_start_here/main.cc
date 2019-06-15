d09a0cf7cdaffbe7a3ea11fb99858dea3cbd1648
Mayank <ogyle@yahoo.com>
2019-06-15 11:02:09 -0700

    Moving executables in examples directory

---
+++ examples/000_start_here/main.cc
@@ -0,0 +1,22 @@
+// Copyright MakeShape. 2019, All rights reserved.
+
+#include "app.hh"
+
+//
+// launch application
+// 
+int main(int argc, char *argv[])
+{
+    makeshape::app::App application;
+    makeshape::spatial::TriMesh m;
+
+    if (argc < 2) {
+        m = makeshape::spatial::load_cube();
+    } else {
+        m = makeshape::spatial::load_mesh(std::string(argv[1]));
+    }
+
+    application.add_mesh(m);
+    application.launch();
+    return 0;
+}
