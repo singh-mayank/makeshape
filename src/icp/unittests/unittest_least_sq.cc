a303a716c9316be55866ee5b2f9e13eb663b3162
Mayank Singh <msingh@aurora.tech>
2019-06-20 11:41:07 -0700

    Finished Least Square. Need to finish unitttests.

---
+++ src/icp/unittests/unittest_least_sq.cc
@@ -0,0 +1,18 @@
+// Copyright MakeShape. 2019, All rights reserved.
+//
+#include "least_sq_rigid_motion.hh"
+#include "trimesh.hh"
+
+#include "gtest/gtest.h" 
+
+TEST(ICP, LeastSQ)
+{
+    EXPECT_EQ(true, true);
+}
+
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
