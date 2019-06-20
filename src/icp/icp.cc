a5fd2c73b51799688e669b1c636544bbcfa9780b
Mayank <ogyle@yahoo.com>
2019-06-19 21:44:54 -0700

    Work on ICP, tomorrow

--- src/icp/icp.cc
+++ src/icp/icp.cc
@@ -5,9 +5,28 @@
 namespace makeshape {
 namespace icp {
 
-void compute_icp() {
+/*
+Eigen::Vector3f trans_vec_A;
+//note that you have to create a Translation because multiplying a 
+//Transform with a vector will _apply_ the transform to the vector
+Eigen::Translation<float,3> translation_A(trans_vec_A);
+Eigen::Quaternionf rotation_B;
+Eigen::Quaternionf rotation_C;
+Eigen::Quaternionf rotation_D;
+Eigen::Vector3f trans_vec_E;
+Eigen::Translation<float,3> translation_E(trans_vec_E);
+Eigen::Transform<float,3,Affine> combined = 
+      translation_A * rotation_B * rotation_C * rotation_D * translation_E;
 
+combined*v = A*B*C*D*E*v = A*(B*(C*(D*(E*v))))
 
+*/
+
+Eigen::Affine3d compute_icp() {
+    //Eigen::Transform<float, 3, Eigen::Affine> combined;
+    
+    Eigen::Affine3d t;
+    return t;
 }
 
 
--- src/icp/icp.hh
+++ src/icp/icp.hh
@@ -1,12 +1,15 @@
 // Copyright MakeShape. 2019, All rights reserved.
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+#include "trimesh.hh"
 
 #pragma once
 
 namespace makeshape {
 namespace icp {
 
-void compute_icp();
+Eigen::Affine3d compute_icp(const spatial::TriMesh &m1, const spatial::TriMesh &m2);
 
 } // icp 
 } // makeshape
