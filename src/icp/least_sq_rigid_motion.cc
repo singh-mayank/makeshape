c87b6fe11ad365e0873ac2bb60fe55caf886a82a
Mayank Singh <msingh@aurora.tech>
2019-06-20 11:48:47 -0700

    Cleanup

--- src/icp/least_sq_rigid_motion.cc
+++ src/icp/least_sq_rigid_motion.cc
@@ -8,26 +8,6 @@
 namespace makeshape {
 namespace icp {
 
-/*
-Eigen::Vector3f trans_vec_A;
-//note that you have to create a Translation because multiplying a 
-//Transform with a vector will _apply_ the transform to the vector
-Eigen::Translation<float,3> translation_A(trans_vec_A);
-Eigen::Quaternionf rotation_B;
-Eigen::Quaternionf rotation_C;
-Eigen::Quaternionf rotation_D;
-Eigen::Vector3f trans_vec_E;
-Eigen::Translation<float,3> translation_E(trans_vec_E);
-Eigen::Transform<float,3,Affine> combined = 
-      translation_A * rotation_B * rotation_C * rotation_D * translation_E;
-
-combined*v = A*B*C*D*E*v = A*(B*(C*(D*(E*v))))
-
-// works
-Eigen::Transform<float, 3, Eigen::Affine> combined;
-*/
-
-
 //
 // Compute Least Square Rigid Motion Using SVD
 // by Olga Sorkine-Hornung and Michael Rabinovich
