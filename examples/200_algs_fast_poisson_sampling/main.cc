e6687cdb809b57eadddd2f867a5bc69ebeea0cf7
Mayank <ogyle@yahoo.com>
2019-07-12 21:52:47 -0700

    Working on poisson sampling

---
+++ examples/200_algs_fast_poisson_sampling/main.cc
@@ -0,0 +1,26 @@
+// Copyright MakeShape. 2019, All rights reserved.
+
+#include "app.hh"
+#include "fast_poisson_sampling.hh"
+
+//
+// launch application
+// 
+int main(int argc, char *argv[])
+{
+    
+    //constexpr double DISTANCE_BETWEEN_SAMPLES = 0.01;
+    //constexpr int N_SAMPLES = 30;
+
+    constexpr double DISTANCE_BETWEEN_SAMPLES = 0.1;
+    constexpr int N_SAMPLES = 30;
+
+    const auto sampled_pts = makeshape::algs::sample_plane(DISTANCE_BETWEEN_SAMPLES, N_SAMPLES);
+    printf("Fast Poisson Sampling. Computed [ %zu ] samples\n", sampled_pts.size());
+
+
+    //makeshape::app::App application;
+    //application.launch();
+
+    return 0;
+}
--- src/algs/fast_poisson_sampling.cc
+++ src/algs/fast_poisson_sampling.cc
@@ -7,21 +7,41 @@ namespace algs {
 
 std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
     struct GridCell {
-        float center;
-        float size;
+        Eigen::Vector2d center;
+        int index;
     };
 
-    constexpr int SIZE = 5;
-    vector<vector<GridCell> grid;
-    grid.resize(5);
+    std::vector<Eigen::Vector2d> ret;
+    constexpr int DIM = 2;
+    const double CELL_SIZE = r/sqrt(DIM);
+    const int SIZE = static_cast<int>(1.0/CELL_SIZE);
+    const double HALF_CELL_SIZE = 0.5 * CELL_SIZE;
+
+    // remove this
+    {
+        printf("Cell size: %f, Size: %i\n", CELL_SIZE, SIZE);
+    }
+
+    std::vector<std::vector<GridCell>> grid;
+    grid.resize(SIZE);
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            
+            const Eigen::Vector2d c((i*CELL_SIZE) + HALF_CELL_SIZE, 
+                                    (j*CELL_SIZE) + HALF_CELL_SIZE);
+            grid[i].emplace_back(GridCell{c, -1});
+            //printf("\t[%i, %i] : GridCell{(%f, %f) | %i}\n", i, j, c[0], c[1], -1);
         }
     }
 
+    auto point_in_cell = [&grid](const Eigen::Vector2d &p) -> bool {
+        return false;
+    };
+
+    std::vector<std::size_t> active_list;
 
 
+
+    return ret;
 }
 
 
