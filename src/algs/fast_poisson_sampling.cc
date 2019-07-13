44cf5bbad9b6af140b6dabc614f1f96858f6dc01
Mayank Singh <msingh@aurora.tech>
2019-07-12 17:01:20 -0700

    Work on fast poisson

--- src/algs/fast_poisson_sampling.cc
+++ src/algs/fast_poisson_sampling.cc
@@ -5,6 +5,26 @@
 namespace makeshape {
 namespace algs {
 
+std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
+    struct GridCell {
+        float center;
+        float size;
+    };
+
+    constexpr int SIZE = 5;
+    vector<vector<GridCell> grid;
+    grid.resize(5);
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            
+        }
+    }
+
+
+
+}
+
+
 std::vector<Eigen::Vector3d> sample_cube(const int n_samples) {
     return std::vector<Eigen::Vector3d>{};
 }
--- src/algs/fast_poisson_sampling.hh
+++ src/algs/fast_poisson_sampling.hh
@@ -8,6 +8,12 @@
 namespace makeshape {
 namespace algs {
 
+// Borrowed from 
+// https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf 
+// @params 'r': the minimum distance between samples
+// @params 'k': limit of samples to choose before rejection
+std::vector<Eigen::Vector2d> sample_plane(const double r, const int k);
+
 std::vector<Eigen::Vector3d> sample_cube(const int n_samples);
 std::vector<Eigen::Vector3d> sample_sphere(const int n_samples);
 
