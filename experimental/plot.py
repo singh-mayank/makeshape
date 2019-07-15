77398182329ade4c698f0341dd9d746d3ab4d590
Mayank Singh <msingh@aurora.tech>
2019-07-15 13:32:06 -0700

    Update python script

--- experimental/plot.py
+++ experimental/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
 
-data = genfromtxt('example.csv', delimiter=',')
+data = genfromtxt('samples.csv', delimiter=',')
 print(data.size)
 print(data.shape)
 
