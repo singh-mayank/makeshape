29502234631c91dc8a6b60bddb46c90536d126d7
Mayank Singh <msingh@aurora.tech>
2019-07-15 12:33:24 -0700

    Add cxxopts and make poisson sampling work

--- examples/200_algs_fast_poisson_sampling/main.cc
+++ examples/200_algs_fast_poisson_sampling/main.cc
@@ -3,34 +3,49 @@
 #include "app.hh"
 #include "fast_poisson_sampling.hh"
 
+#include <cxxopts.hpp>
+
 //
 // launch application
 // 
 int main(int argc, char *argv[])
 {
     
-    //constexpr double DISTANCE_BETWEEN_SAMPLES = 0.01;
-    //constexpr int N_SAMPLES = 30;
+    cxxopts::Options options("Fast Poisson Sampling example", 
+            "Generates a set of random samples on a 2d plane (XY), with bounds [0.0, 1.0]");
 
-    constexpr double DISTANCE_BETWEEN_SAMPLES = 0.1;
-    constexpr int N_SAMPLES = 30    ;
+    options.add_options()
+        ("d,debug", "Enable debugging", 
+         cxxopts::value<bool>()->default_value("false"))
+        ("n,samples", "Number of samples, defaulted to 30", 
+         cxxopts::value<int>()->default_value("30"))
+        ("m,dist", "Distance between samples, defaulted to 0.04", 
+         cxxopts::value<double>()->default_value("0.04"))
+        ("f,output_filename", "Write points to filename", 
+         cxxopts::value<std::string>()->default_value(""))
+        ;
+    const auto result = options.parse(argc, argv);
 
+    const double DISTANCE_BETWEEN_SAMPLES = result["dist"].as<double>();
+    const int N_SAMPLES = result["samples"].as<int>();
+
+    printf("Fast Poisson Sampling on 2d plane, with bounds [0.0, 1.0]\n");
+    printf("\t N_SAMPLES: %i | Distance between samples: %f\n", 
+            N_SAMPLES, 
+            DISTANCE_BETWEEN_SAMPLES);
     const auto sampled_pts = makeshape::algs::sample_plane(DISTANCE_BETWEEN_SAMPLES, N_SAMPLES);
-    printf("Fast Poisson Sampling. Computed [ %zu ] samples\n", sampled_pts.size());
+    printf("\t Computed [ %zu ] samples\n", sampled_pts.size());
 
-	{
-		std::ofstream myfile;
-		myfile.open("example.csv");
-		for (const auto &each : sampled_pts) {
-			myfile << each.x() << "," << each.y() << "\n";
-		}
-		myfile.close();
-	}
-
-
-
-    //makeshape::app::App application;
-    //application.launch();
+    const bool debug = result["debug"].as<bool>();
+    const std::string filename = result["output_filename"].as<std::string>();
+    if (debug && filename != "") {
+        std::ofstream myfile;
+        myfile.open(filename);
+        for (const auto &each : sampled_pts) {
+            myfile << each.x() << "," << each.y() << "\n";
+        }
+        myfile.close();
+    }
 
     return 0;
 }
--- src/algs/fast_poisson_sampling.cc
+++ src/algs/fast_poisson_sampling.cc
@@ -13,7 +13,7 @@ namespace {
 
 constexpr double R = 1.0/static_cast<double>(RAND_MAX);
 double generate_random(const double min_value, const double max_value) {
-    double random_number = rand() * R; // between 0 and 1
+    double random_number = rand() * R; // between 0 and 1, included
     return (min_value + (random_number * (max_value - min_value)));
 }
 
@@ -27,10 +27,10 @@ std::vector<Eigen::Vector2d> generate_random_pts(const double r,
         // generate random point
         double theta = generate_random(0, 2*M_PI);
         double radius = generate_random(r2, r4);
-		Eigen::Vector2d p = c + Eigen::Vector2d{ radius * cos(theta), radius * sin(theta) };
-		if (p.x() < 0.0 || p.x() > 1.0 || p.y() < 0.0 || p.y() > 1.0) {
-			continue;
-		}
+        Eigen::Vector2d p = c + Eigen::Vector2d{ radius * cos(theta), radius * sin(theta) };
+        if (p.x() < 0.0 || p.x() > 1.0 || p.y() < 0.0 || p.y() > 1.0) {
+            continue;
+        }
         ret.push_back(p);
     }
     return ret;
@@ -40,7 +40,6 @@ std::vector<Eigen::Vector2d> generate_random_pts(const double r,
 
 std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
     struct GridCell {
-        Eigen::Vector2d center;
         int index; // Index into an array of samples. 
                    // Sample point lies within this GridCell
     };
@@ -51,19 +50,11 @@ std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
     const int SIZE = static_cast<int>(1.0/CELL_SIZE);
     const double HALF_CELL_SIZE = 0.5 * CELL_SIZE;
 
-    // remove this
-    {
-        printf("Cell size: %f, Size: %i\n", CELL_SIZE, SIZE);
-    }
-
     std::vector<std::vector<GridCell>> grid;
     grid.resize(SIZE);
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
-            const Eigen::Vector2d c((i*CELL_SIZE) + HALF_CELL_SIZE, 
-                                    (j*CELL_SIZE) + HALF_CELL_SIZE);
-            grid[i].emplace_back(GridCell{c, -1});
-            //printf("\t[%i, %i] : GridCell{(%f, %f) | %i}\n", i, j, c[0], c[1], -1);
+            grid[i].emplace_back(GridCell{-1});
         }
     }
     
@@ -71,11 +62,11 @@ std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
     std::vector<std::size_t> active_list;
 
     auto insert = [&SIZE, &grid, &active_list, &samples](const Eigen::Vector2d &p) {
-        const int row = static_cast<int>(SIZE * p.x());
-        const int col = static_cast<int>(SIZE * p.y());
         samples.emplace_back(p);
         const int last_index = static_cast<int>(samples.size()-1);
         active_list.push_back(last_index);
+        const int row = static_cast<int>(SIZE * p.x());
+        const int col = static_cast<int>(SIZE * p.y());
         grid[row][col].index = last_index;
     };
 
@@ -83,16 +74,17 @@ std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
         const double r2 = (r*r);
         const int row = static_cast<int>(SIZE * p.x());
         const int col = static_cast<int>(SIZE * p.y());
-        for(int r = row-1; r < row+1; ++r) {
-            for(int c = col-1; c < col+1; ++c) {
+        const int offset = 1;
+        for(int r = (row - offset); r <= (row + offset); ++r) {
+            for(int c = (col - offset); c <= (col + offset); ++c) {
                 if (r < 0 || r >= SIZE || c < 0 || c >= SIZE) {
                     continue;
                 }
 
-				const int index = grid[r][c].index;
-				if (index < 0 || index >= samples.size()) {
-					continue;
-				}
+                const int index = grid[r][c].index;
+                if (index < 0 || index >= samples.size()) {
+                    continue;
+                }
 
                 Eigen::Vector2d p_neighbour = samples.at(index);
                 if((p - p_neighbour).squaredNorm() < r2) {
@@ -103,17 +95,13 @@ std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
         return true;
     };
 
-
-    Eigen::Vector2d x0{rand()*R, rand()*R};
-	CHECK(x0.x() <= 1.0);
-	CHECK(x0.x() >= 0.0);
-	CHECK(x0.y() <= 1.0);
-	CHECK(x0.y() >= 0.0);
-
+    Eigen::Vector2d x0{generate_random(0.0, 1.0), generate_random(0.0, 1.0)};
     insert(x0);
+    int count = 0;
     while (!active_list.empty()) {
-        int random_index = static_cast<int>((rand()*R) * active_list.size()-1);
-        CHECK(random_index < active_list.size());
+        const double min_value = 0.0;
+        const double max_value = (active_list.size() - 1.0);
+        int random_index = static_cast<int>(generate_random(min_value, max_value));
     
         // generate 'k' points in annulus of 'r' to '2r'
         const Eigen::Vector2d &xi = samples.at(active_list.at(random_index));
@@ -128,12 +116,11 @@ std::vector<Eigen::Vector2d> sample_plane(const double r, const int k) {
             } 
         }
 
+        //  remove from active list
         if (!sample_found) {
             active_list.erase(active_list.begin() + random_index);
         }
-
     }
-
     return samples;
 }
 
